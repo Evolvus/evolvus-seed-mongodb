83aa7c89e6692cdd4dc045a475e99e9a45ceacee
user <user@elpblr-083.evolvus.com>
2018-11-29 12:38:35 +0530

    added env variable for seed scripts

--- fileConfig.js
+++ fileConfig.js
@@ -1,63 +1,64 @@
 const path = require('path');
 const dir = process.env.SCRIPT_HOME || '/data/SA/Script';
-var k=path.resolve(__dirname,'./data');
+var k = path.resolve(__dirname, './data');
+var scriptsPath = process.env.SCRIPT_PATH || k;
 
 module.exports.configure = [{
-  template: `${k}/applications.vm`,
+  template: `${scriptsPath}/applications.vm`,
   fileName: 'applications.json',
   outputdir: `${dir}`,
   context: './context.js',
   output: `${dir}/applications.json`
 },
 {
-  template: `${k}/entities.vm`,
+  template: `${scriptsPath}/entities.vm`,
   fileName: 'entities.json',
   outputdir: `${dir}`,
   context: './context.js',
   output: `${dir}/entities.json`
 
 }, {
-  template: `${k}/mastercurrencies.vm`,
+  template: `${scriptsPath}/mastercurrencies.vm`,
   fileName: 'mastercurrencies.json',
   outputdir: `${dir}`,
   context: './context.js',
   output: `${dir}/mastercurrencies.json`
 }, {
-  template: `${k}/mastertimezones.vm`,
+  template: `${scriptsPath}/mastertimezones.vm`,
   fileName: 'mastertimezones.json',
   outputdir: `${dir}`,
   context: './context.js',
   output: `${dir}/mastertimezones.json`
 },
 {
-  template: `${k}/menus.vm`,
+  template: `${scriptsPath}/menus.vm`,
   fileName: 'menus.json',
   outputdir: `${dir}`,
   context: './context.js',
   output: `${dir}/menus.json`
 }, {
-  template: `${k}/roles.vm`,
+  template: `${scriptsPath}/roles.vm`,
   fileName: 'roles.json',
   context: './context.js',
   outputdir: `${dir}`,
   output: `${dir}/roles.json`
 },
 {
-  template: `${k}/supporteddateformats.vm`,
+  template: `${scriptsPath}/supporteddateformats.vm`,
   fileName: 'supporteddateformats.json',
   context: './context.js',
   outputdir: `${dir}`,
   output: `${dir}/supporteddateformats.json`
 },
 {
-  template: `${k}/lookups.vm`,
+  template: `${scriptsPath}/lookups.vm`,
   fileName: 'lookups.json',
   outputdir: `${dir}`,
   context: './context.js',
   output: `${dir}/lookups.json`
 },
 {
-  template: `${k}/swesetups.vm`,
+  template: `${scriptsPath}/swesetups.vm`,
   fileName: 'swesetups.json',
   context: './context.js',
   outputdir: `${dir}`,
